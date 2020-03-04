5c3ad540f0ce9b28c2b9eacd78856e0ebae361fb
rachelwen <rachelwen823@gmail.com>
2020-03-03 19:28:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+
+
 function setup() {
     createCanvas(400, 600);
   }
@@ -5,7 +7,8 @@ function setup() {
   function draw() {
     noStroke()
     background(242, 207, 187);
-    fill( 68, 166, 156)
+    fillRandomColor();
+   
     beginShape()
     vertex(0,0)
     vertex(0,350)
@@ -14,7 +17,8 @@ function setup() {
     
     rect(200,450,200,100)
     
-    fill(89, 46, 21)
+   
+   fillRandomColor();
     beginShape()
     vertex(0,350)
     vertex(200,350)
@@ -23,7 +27,8 @@ function setup() {
     endShape()
       rect(0,550,width,50)
     
-    fill(140, 103, 81)
+  
+   fillRandomColor();
     beginShape()
     vertex(200,0)
     vertex(400,0)
@@ -32,20 +37,48 @@ function setup() {
     endShape()
     rect(0,450,200,100)
     
-    fill(191, 156, 136)
+  
+   fillRandomColor();
     arc(300,90,200,200,PI,0)
     rect(0,350,200,100)
     
-    fill(242, 207, 187)
+    fillRandomColor();
     arc(100,450,200,200,0,PI)
     
    fill(255)
     textFont('helvetica')
     textSize(28)
-    text('loren', -35+width/2,-80+height/2)
+    text('loren', -65+width/2,-80+height/2)
     textAlign(CENTER)
    
     text('ipsum',40+width/2,-100+height/2)
-    
+    noLoop();
   }
-  
+      ///this is kind of a redundant function. im realizing 
+      // i could have used an array here!. but storing sets of 3 numbers in an array sounds a bit tricky to me.
+      // so very long if-statement it is.
+  function fillRandomColor(){
+    let randomNum = floor(random(1,10))
+    if (randomNum == 1){
+        fill( 68, 166, 156)
+         }else if(randomNum == 2){
+            fill(89, 46, 21)
+         }else if(randomNum == 3){
+            fill(140, 103, 81)
+         }else if(randomNum == 4){
+            fill(191, 156, 136)
+         }else if(randomNum == 5){
+            fill(242, 207, 187)
+         }else if(randomNum == 6){
+             fill(166, 75, 41)
+         }else if(randomNum == 7){
+             fill(242, 192, 41)
+         }else if(randomNum == 8){
+             fill(191, 161, 143)
+         }else if(randomNum == 9){
+             fill(64, 60, 38)
+         }else{
+             fill(115, 96, 61)
+         }
+
+  }
